cbbdb87b4a7f017e0a9f3846f1cc1c64dbd02cb6
bitbaba <imzhhwu@gmail.com>
2017-12-14 14:08:18 +0800

    local build

--- travis.sh
+++ travis.sh
@@ -195,7 +195,7 @@ fi
 
 make distdir VERSION=$HOST
 
-cd bitcoin-$HOST
+cd bitgold-$HOST
 
 ./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && exit 1)
 make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && make $GOAL V=1 ; exit 1 )
