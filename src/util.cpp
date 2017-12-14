d39941e01e03799df75ddfcefe6a6bc483450ec7
bitbaba <imzhhwu@gmail.com>
2017-12-14 11:06:16 +0800

    default data directory to bitgold;

--- src/util.cpp
+++ src/util.cpp
@@ -544,7 +544,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitgold";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -554,10 +554,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Bitgold";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".bitgold";
 #endif
 #endif
 }
