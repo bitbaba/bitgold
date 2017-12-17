6061d7f9507f5b16a7ebbd1a8030cbd81371ab9c
bitbaba <imzhhwu@gmail.com>
2017-12-17 14:56:16 +0800

    protect the childhood;

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -10,7 +10,7 @@
 #include <stdint.h>
 
 /** The maximum allowed size for a serialized block (consensus is NO limit) */
-static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 0xffffFFFF;
+static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 8000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
 static const unsigned int MAX_BLOCK_WEIGHT = MAX_BLOCK_SERIALIZED_SIZE;
 /** The maximum allowed number of signature check operations in a block (network rule) */
