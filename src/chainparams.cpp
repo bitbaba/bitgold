dd9942c5b2ec0290c01e2224a117b044b36553d9
bitbaba <imzhhwu@gmail.com>
2018-01-05 20:47:15 +0800

    no service filter yet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -146,7 +146,7 @@ public:
         bech32_hrp = "BTG";
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.emplace_back("seed.bitgold.bitbaba.com", true);
+        vSeeds.emplace_back("seed.bitgold.bitbaba.com", false);
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
