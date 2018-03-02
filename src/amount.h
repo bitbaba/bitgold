e26364b639c3bcf8885aa60efbb233a0add85901
bitbaba <imzhhwu@gmail.com>
2018-03-02 16:06:47 +0800

    remove unused units

--- src/amount.h
+++ src/amount.h
@@ -11,9 +11,6 @@
 /** Amount in satoshis (Can be negative) */
 typedef int64_t CAmount;
 
-static const CAmount UNIT = 1;
-static const CAmount GRAM = 100000000 * UNIT;
-
 static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
