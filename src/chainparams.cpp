bd2da0a8d7c86601425f7f6e00d65097a6a0596b
bitbaba <imzhhwu@gmail.com>
2018-03-07 13:52:01 +0800

    add checkpoint:1008

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -180,7 +180,7 @@ public:
         checkpointData = {
             {
                 {     0, HASH_GENESIS},
-                //{  1008, uint256S("0x0000060391664ef2eeb2a08844da5281a729def0b266dc18452fbf3569ae7443")},
+                {  1008, uint256S("0x00000ff673327dcbfbf65f13a594345bafd6804ad08eac1749939d01989a088d")},
                 //{  2016, uint256S("0x000001a9574b41f85d960049a51d7180c901b6db493e53b379507bb19cc1bcbc")},
             }
         };
